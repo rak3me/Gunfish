f41aadc6f5a09a1a95b315d9ce153cfb46ca4308
Ryan Kann <rak3me@virginia.edu>
2018-05-20 21:39:48 -0400

    Started Geometry Override Stuff pretty cool imo

---
+++ Gunfish Unity/Assets/GunfishSpriteHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunfishSpriteHandler : MonoBehaviour {
+
+	Sprite sprite;
+
+	// Use this for initialization
+	void Start () {
+		sprite = GetComponent<SpriteRenderer> ().sprite;
+		Vector2[] vertices = new Vector2[]{ new Vector2(-1f, 1f), new Vector2(1f, 1f), new Vector2(-0.5f, -1f), new Vector2(0.5f, -1f) };
+		sprite.OverrideGeometry (vertices, sprite.triangles);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
